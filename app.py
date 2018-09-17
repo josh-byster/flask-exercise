e93e11cf5142729a34a168b2f983f5f801e0b453
Josh Byster <joshb1050@gmail.com>
2018-09-17 00:58:34 -0500

    Added unit tests

--- app.py
+++ app.py
@@ -56,34 +56,35 @@ def user():
     if request.method == 'GET':
         team = request.args.get('team')
         if team is not None:
-            return get_users_by_team(team)
+            return getUsersByTeam(team)
         else:
             # If no team specified as a query param, return all the users
-            return get_all_users()
+            return getAllUsers()
     if request.method == 'POST':
         # Create a new user to update the DB
-        return add_user(request.args.to_dict())
+        return addUser(request.args.to_dict())
 
-def get_all_users():
+def getAllUsers():
     users = db.get("users")
     data = {"users": users}
     return create_response(data)
 
-def get_users_by_team(team):
+def getUsersByTeam(team):
     users_on_team = db.getByTeam("users",team)
     data = {"users": users_on_team}
     return create_response(data)
 
-def add_user(user_data):
+def addUser(user_data):
     if "name" in user_data and "age" in user_data and "team" in user_data:
         # Create the user since all the necessary fields exist
+        user_data["age"] = int(user_data["age"])
         new_user = db.create("users",user_data)
         return create_response({"user":new_user},status=201)
     # Return an error otherwise    
     message = "Cannot create new user with missing parameters. Got name={0},age={1},team={2}".format(user_data.get("name"),user_data.get("age"),user_data.get("team"))
     return create_response(message=message,status=422)
     
-def update_user(user_id,update_values):
+def updateUser(user_id,update_values):
     if db.getById("users",int(user_id)) is not None:
         user_update = db.updateById("users",int(user_id),update_values)
         return create_response(user_update)
@@ -101,7 +102,7 @@ def deleteUser(user_id):
         return create_response(message=message,status=404)
     
 @app.route("/users/<user_id>", methods = ["GET","PUT","DELETE"])
-def user_by_id(user_id):
+def userById(user_id):
     if request.method == "GET":
         data_for_user = db.getById("users",int(user_id))
         if data_for_user is not None:
@@ -111,7 +112,7 @@ def user_by_id(user_id):
             data = {"message": "A user with id {0} was not found in the database.".format(user_id)}
             return create_response(data, status = 404)
     if request.method == "PUT":
-        return update_user(user_id,request.args.to_dict())
+        return updateUser(user_id,request.args.to_dict())
     if request.method == "DELETE":
         return deleteUser(user_id)
 
--- test_app.py
+++ test_app.py
@@ -39,3 +39,76 @@ def test_get_user_id(client):
     res_user = res.json["result"]["user"]
     assert res_user["name"] == "Aria"
     assert res_user["age"] == 19
+
+# Adds a new user to the database
+def test_add_new_user(client):
+    res = client.post("/users?team=LWB&name=Josh&age=19")
+    assert res.status_code == 201
+
+    res_user = res.json["result"]["user"]
+    assert res_user["name"] == "Josh"
+    assert res_user["age"] == 19
+    assert res_user["team"] == "LWB"
+
+# Checks that the new user is listed when querying teams
+def test_new_user_on_team(client):
+    res = client.get("/users?team=LWB")
+    assert res.status_code == 200
+
+    res_users = res.json["result"]["users"]
+    assert len(res_users) == 3
+    assert res_users[2]["name"] == "Josh"
+
+# Test to make sure that now the user count is 5
+def test_get_users_after_add(client):
+    res = client.get("/users")
+    assert res.status_code == 200
+
+    res_users = res.json["result"]["users"]
+    assert len(res_users) == 5
+    assert res_users[4]["name"] == "Josh"
+
+# Test that the we get a 422 response if not all parameters are in the request
+def test_add_invalid_user(client):
+    res = client.post("/users?team=LWB&name=Josh")
+    assert res.status_code == 422
+
+# Update a user record and check it gives a valid response
+def test_put_user(client):
+    res = client.put("/users/2?team=FAR")
+    assert res.status_code == 200
+
+# Test that the PUT request above persisted to the database
+def test_put_persists(client):
+    res = client.get("/users")
+
+    res_users = res.json["result"]["users"]
+    assert len(res_users) == 5
+    assert res_users[1]["team"] == "FAR"
+
+# Attempt to update a user id that doesn't exist
+def test_put_invalid_user(client):
+    res = client.put("/users/9?team=FAR")
+    assert res.status_code == 404
+
+# Test that we can delete a user in our database
+def test_delete_user(client):
+    res = client.delete("/users/1")
+    assert res.status_code == 200
+
+    get_res = client.get("/users")
+
+    res_users = get_res.json["result"]["users"]
+    assert len(res_users) == 4
+    assert res_users[0]["id"] == 2
+
+# Ensure that we don't raise an exception if we try deleting an invalid user
+# and that we didn't delete another user unintentionally
+def test_delete_invalid_user(client):
+    res = client.delete("/users/9")
+    assert res.status_code == 404
+
+    get_res = client.get("/users")
+
+    res_users = get_res.json["result"]["users"]
+    assert len(res_users) == 4
